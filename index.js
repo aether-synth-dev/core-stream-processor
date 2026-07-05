386d20f7e094a828410cc45e47f5f415acd7b0e0
Jacob Molak <aethersynthdev@gmail.com>
2026-07-05 15:36:55 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+// File: index.js
+/**
+ * Core Stream Processor
+ * 
+ * A production-ready Node.js module that processes data streams with integrated
+ * telemetry and sanitization capabilities following Clean Architecture principles.
+ * 
+ * @module core-stream-processor
+ */
+
+const Processor = require('./lib/processor');
+
+module.exports = Processor;
